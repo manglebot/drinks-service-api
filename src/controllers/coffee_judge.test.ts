48704474c330aa6c3aaad56364274551f2df2e8a
Ben <manglebot@users.noreply.github.com>
2023-11-21 17:25:24 +0000

    fix: moved GET test for /coffeelover endpoint to own test file

---
+++ src/controllers/coffee_judge.test.ts
@@ -0,0 +1,9 @@
+// As a user, when I do a GET request to the /coffeelover endpoint, I will see a string message as a response which says I like coffee!
+
+describe('Test /coffeelover endpoint request', () => {
+    test('GET should return correct message', async () => {
+      const res = await request(app).get('/coffeelover');
+      expect(res.statusCode).toEqual(200);
+      expect(res.text).toEqual('I like coffee!');
+  }); });
+
