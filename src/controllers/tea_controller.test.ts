d0e93ed7820f639034c16e2867def2e2a9203c72
Ben <manglebot@users.noreply.github.com>
2023-11-21 22:42:48 +0000

    feat:add test for teaName not supplied

--- src/controllers/tea_controller.test.ts
+++ src/controllers/tea_controller.test.ts
@@ -12,3 +12,14 @@ test('GET /tea should return correct object', async () => {
     name: 'Earl Grey',
   });
 });
+
+test('GET /tea teaName not supplied so default should be returned ', async () => {
+  const res = await request(app)
+  .get('/tea')
+
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: 'Tea',
+    name: 'Earl Grey',
+  });
+});
