077079c9c1a61119bf24a0ca4cfcb4506cc267d6
Ben <manglebot@users.noreply.github.com>
2023-11-21 21:01:50 +0000

    feat: add unit test for when no coffee name is supplied

--- src/controllers/coffee_controller.test.ts
+++ src/controllers/coffee_controller.test.ts
@@ -5,10 +5,21 @@ test('GET /coffee should return correct object', async () => {
   const res = await request(app)
   .get('/coffee')
   .query({ coffeeName: 'Latte' });
-  
+
   expect(res.statusCode).toEqual(200);
   expect(res.body).toEqual({
     drinkType: 'Coffee',
     name: 'Latte',
   });
 });
+
+test('GET /coffee coffeeName not supplied so default should be returned ', async () => {
+    const res = await request(app)
+    .get('/coffee')
+  
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+      drinkType: 'Coffee',
+      name: 'Latte',
+    });
+  });
