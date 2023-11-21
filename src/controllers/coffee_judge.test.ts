da40e5f78b7d942b2d7d155dbbe6d66c90288c58
Ben <manglebot@users.noreply.github.com>
2023-11-21 20:55:42 +0000

    feat: add test for different endpoint url

--- src/controllers/coffee_judge.test.ts
+++ src/controllers/coffee_judge.test.ts
@@ -1,9 +1,6 @@
 import request from 'supertest';
 import { app } from '../app';
 
-
-// As a user, when I do a GET request to the /coffeelover endpoint, I will see a string message as a response which says I like coffee!
-
 describe('Test /coffeelover endpoint request', () => {
     test('GET should return correct message', async () => {
       const res = await request(app).get('/coffeelover');
@@ -11,3 +8,11 @@ describe('Test /coffeelover endpoint request', () => {
       expect(res.text).toEqual('I like coffee!');
   }); });
 
+  describe('Test /dracula endpoint request', () => {
+    test('GET should return correct message', async () => {
+      const res = await request(app).get('/dracula');
+      expect(res.statusCode).toEqual(200);
+      expect(res.text).toEqual("I don't drink... coffee.");
+  }); });
+
+  
